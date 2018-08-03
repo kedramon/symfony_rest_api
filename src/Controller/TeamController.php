ec4db15715ef148012b0341c3659bfec4c2264c8
Volodymyr Kenidra <volodymyr.kenidra@lemberg.co.uk>
2018-08-03 14:51:49 +0300

    Add phpunit test bundle. Fix unused class.

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
